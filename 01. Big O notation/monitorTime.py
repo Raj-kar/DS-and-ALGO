71595982bd8e9e0975b42d7f9470ab21cf3af0e5
Raj <rajkar921@gmail.com>
2021-04-29 22:15:44 +0530

    monitor time decorator added

--- 01. Big O notation/monitorTime.py
+++ 01. Big O notation/monitorTime.py
@@ -1,3 +1,4 @@
+# A decorator for monitor total times take a function to get execute
 import time
 
 
@@ -10,25 +11,3 @@ def monitorTime(func):
         print(round(end_time - start_time, 2), "sec")
         return total
     return wrapper
-
-# Iterative method !
-# Time complexity - O(n)
-@monitorTime
-def addUpTo(n):
-    total = 0
-    for i in range(1, n+1):
-        total += i
-    return total
-
-
-print(addUpTo(999999999))
-
-
-# Using Formula.
-# Time complexity - O(1)
-@monitorTime
-def addUpTo(n):
-    return n*(n+1) // 2
-
-
-print(addUpTo(999999999))
